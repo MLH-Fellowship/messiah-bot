4e0aaf85976b9b6e757f033c1fe4f15a66dc5cec
Aanand Kainth <aanand@akainth.me>
2021-03-11 01:01:30 -0800

    Remove messiah bot key

--- messiah/__main__.py
+++ messiah/__main__.py
@@ -1,6 +1,7 @@
 from messiah import MessiahClient
+import os
 
 if __name__ == '__main__':
     client = MessiahClient()
     # noinspection SpellCheckingInspection
-    client.run("ODE5NDYzMTgwMjc4NTYyODQ3.YEm-jw.pzEUJjzHFUEM9XPmq-OSSIbmfNs")
+    client.run(os.getenv("messiah_bot"))

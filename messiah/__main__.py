3351107ac3b48087e747b00609bc51a1bf203f98
Drew Ehrlich <47901184+deehrlic@users.noreply.github.com>
2021-03-11 21:35:19 -0800

    remove token

--- messiah/__main__.py
+++ messiah/__main__.py
@@ -1,8 +1,6 @@
 from messiah import MessiahClient
 import os
 
-os.environ["messiah_bot"]="ODE5NDYzMTgwMjc4NTYyODQ3.YEm-jw.AAx2Ntc_RizLOOmED9NmjnAbMaI"
-
 
 if __name__ == '__main__':
     client = MessiahClient()
